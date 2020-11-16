ba5b8c91cc3e9ffeb10270dd7be3adbc2aa7ea33
Andairka <andairka@wp.pl>
2020-11-16 13:24:43 +0100

    Dodanie app modulu

---
+++ app/src/main/java/com/andairka/wsb/tota/MainActivity.java
@@ -0,0 +1,17 @@
+package com.andairka.wsb.tota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kostka.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
